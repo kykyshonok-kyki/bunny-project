2d0a195f064371cbe5c7cbab81b87d570a51021a
kyki <lifekyki@gmail.com>
2020-04-17 19:03:59 +0300

    Add Reset button

---
+++ Assets/Scripts/ButtonsOnClick.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonsOnClick : MonoBehaviour
+{
+    public void Reset()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
