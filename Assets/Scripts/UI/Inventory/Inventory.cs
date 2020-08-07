9163cf97025a62c9c1246643c0124bcb2aba1428
MrNordwind <62465092+MrNordwind@users.noreply.github.com>
2020-08-07 14:07:22 +0300

    InvButton switches inventory activeness

---
+++ Assets/Scripts/UI/Inventory/Inventory.cs
@@ -0,0 +1,28 @@
+﻿//made by Nord
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+    public GameObject inventoryButton;
+    
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void SwitchInv()
+    {
+        if (gameObject.activeSelf == false)
+        {
+            gameObject.SetActive(true);
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+    
+}
