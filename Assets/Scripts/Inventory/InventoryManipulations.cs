167d6abc12e61d3c07d2dd7e2f47f1311c8b438b
MrNordwind <62465092+MrNordwind@users.noreply.github.com>
2020-08-25 22:41:00 +0300

    minor changes

---
+++ Assets/Scripts/Inventory/InventoryManipulations.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManipulations : MonoBehaviour
+{
+
+    public InventoryObject characterInventory;
+    public InventoryObject lootedInventory;
+    
+    public GameObject displayedCharacterInventory;
+    public GameObject displayedLootedInventory;
+
+
+
+    public void checkCurrentInventories()
+    {
+        characterInventory = displayedCharacterInventory.GetComponent<DisplayInventory>().inventory;
+        lootedInventory = displayedLootedInventory.GetComponent<DisplayInventory>().inventory;
+    }
+
+    public void pressedTakeButton(int _i)
+    {
+        var _item = lootedInventory.Container.Items[_i].item;
+
+        characterInventory.AddItem(_item, 1);
+        lootedInventory.RemoveItem(_i);
+
+    }
+
+    public void pressedRemoveButton(int _i)
+    {
+        var _item = lootedInventory.Container.Items[_i].item;
+
+        characterInventory.RemoveItem(_i);
+        lootedInventory.AddItem(_item, 1);
+    }
+}
